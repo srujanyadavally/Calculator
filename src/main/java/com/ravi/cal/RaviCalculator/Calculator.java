fb7cdaa4f859692b9f8f1b4c758dc441043b00e5
srujanyadavally <srujan.yadavally@outlook.in>
2020-04-06 04:39:36 +0000

    product bug fixed

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -1,4 +1,4 @@
-package com.ravi.cal.RaviCalculator;
+upackage com.ravi.cal.RaviCalculator;
 
 import java.util.Scanner;
 
@@ -35,7 +35,7 @@ public class Calculator  {
 	
 	public long mulFucn(long first, long second){
 		
-		return first/second;
+		return first*second;
 	}
 }
 
